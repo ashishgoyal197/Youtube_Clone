5328d361ccea8fb740cded19646cd153f26eee93
Ashish Goyal <ashish.goyal197@gmail.com>
2024-07-08 10:07:29 +0530

    setup and fetch data from api

---
+++ youtube_clone/src/App.jsx
@@ -0,0 +1,31 @@
+import { AppContext } from "./context/contextApi.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header.jsx";
+import Feed from "./components/Feed.jsx";
+import VideoDetails from "./components/VideoDetails.jsx";
+import SearchResult from "./components/SearchResult.jsx";
+
+function App() {
+  return (
+    <div>
+      <AppContext>
+        <BrowserRouter>
+          <div className="flex flex-col h-full">
+            <Header />
+            <Routes>
+              <Route path="/" exact element={<Feed />} />
+              <Route
+                path="/searchReasult/:searchQuery"
+                element={<SearchResult />}
+              />
+              <Route path="/video/:id" element={<VideoDetails />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AppContext>
+    </div>
+  );
+}
+
+export default App;
